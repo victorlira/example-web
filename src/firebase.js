639488259c2723519d5ba09237fcafa0536d381b
antonio <antonio.rec@gmail.com>
2024-11-18 09:22:51 -0300

    fixing firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 let config = {
-    apiKey: "AIzaSyCVBq59SxQad8ULVCwHnWYEGC85BqUAHP0",
-    authDomain: "example-c206c.firebaseapp.com",
-    databaseURL: "https://example-c206c-default-rtdb.firebaseio.com",
-    projectId: "example-c206c",
-    storageBucket: "example-c206c.firebasestorage.app",
-    messagingSenderId: "624485683135",
-    appId: "1:624485683135:web:0ec547586675771ffa5b0a",
-    measurementId: "G-74FBY11Q0G"
+    /* put project data here */
 };
 
 const firebaseApp = initializeApp(config);
