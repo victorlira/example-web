4ef5e68279d59b5d8405b2c606a0af49d20b73b0
Victor <vl@cin.ufpe.br>
2024-12-15 11:04:50 -0300

    firestore example

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 let config = {
-    /* put project data here */
+    apiKey: "AIzaSyCVBq59SxQad8ULVCwHnWYEGC85BqUAHP0",
+    authDomain: "example-c206c.firebaseapp.com",
+    databaseURL: "https://example-c206c-default-rtdb.firebaseio.com",
+    projectId: "example-c206c",
+    storageBucket: "example-c206c.firebasestorage.app",
+    messagingSenderId: "624485683135",
+    appId: "1:624485683135:web:0ec547586675771ffa5b0a",
+    measurementId: "G-74FBY11Q0G"
 };
 
 const firebaseApp = initializeApp(config);
